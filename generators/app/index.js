d89711bc62b36e422f2fec81d97d922339b5d08d
Yarik Genza <gsmeter@gmail.com>
2018-01-04 22:46:50 +0200

    extend scenario

--- generators/app/index.js
+++ generators/app/index.js
@@ -13,12 +13,17 @@ const migrationsWriter = require('./writers/migrations.writer');
 const appWriter = require('./writers/app.writer');
 
 module.exports = class extends Generator {
-  prompting() {
+  async prompting() {
+
+    const mergeProps = (data) => {
+      this.props = Object.assign(this.props, data);
+    }
 
     this.log(yosay(`Welcome to ${chalk.red('restifizer')} generator!`));
-    this.log('Let me know a bit more about you new project ;)\n');
+    this.log(chalk.red('Let me know a bit more about you new project ;)\n'));
 
-    const prompts = [{
+    /* General project infogration */
+    const infoPrompts = [{
       type: 'input',
       name: 'name',
       message: 'What is your project name?',
@@ -29,13 +34,62 @@ module.exports = class extends Generator {
       message: 'What is your project description?',
       default: 'restifizer-app',
     }];
+    this.props = await this.prompt(infoPrompts);
 
-    return this.prompt(prompts).then(props => {
-      this.props = props;
+    /* Transport conf. TO-DO: write the appropriate conditions to the template */
+    this.log(chalk.red(`\nOk. Now let's configure restifizer for ${this.props.name}\n`));
+
+    const rfProps = await this.prompt({
+      type: 'confirm',
+      name: 'addSocket',
+      message: 'Restifizer comes with Express transport by default. Would you like to add socket.io additionaly?',
+      default: false,
+    });
+    mergeProps(rfProps);
+
+    /* Auth plugin conf.  TO-DO: write the appropriate conditions to the template */
+    const authProps = await this.prompt({
+      type: 'confirm',
+      name: 'useSNAuth',
+      message: 'Should I configure auth with social networks?',
+      default: true,
     })
+    mergeProps(authProps);
+
+    /* Get a list of SN to integrate TO-DO: write the appropriate conditions to the template */
+    if (this.props.useSNAuth) {
+      const SNAuthProps = await this.prompt({
+        type: 'checkbox',
+        name: 'snList',
+        choices: ['facebook', 'google', 'vk'],
+        message: 'Select social networks which you need to integrate',
+        default: ['facebook']
+      });
+      mergeProps(SNAuthProps);
+    }
+
+    /* i18n TO-DO: write the appropriate conditions to the template */
+    const i18nProps = await this.prompt({
+      type: 'confirm',
+      name: 'add_i18n',
+      message: 'Would you like to include i18n?',
+      default: true,
+    })
+    mergeProps(i18nProps);
+
+    if (this.props.add_i18n) {
+      const defaultLocale = await this.prompt({
+        type: 'input',
+        name: 'defaultLocale',
+        message: 'Type a default locale to use',
+        default: 'en',
+      });
+      mergeProps(defaultLocale);
+    }
   }
 
   writing() {
+    this.log(chalk.red(`\nAll right! Now I will generate required files (press ${chalk.yellow('Ctrl + C to abort')})\n`));
     rootWriter.call(this);
     apidocsWriter.call(this);
     configWriter.call(this);
@@ -54,7 +108,7 @@ module.exports = class extends Generator {
   }
 
   end() {
-    this.log(`\nOk, now everything is ${chalk.green('ready')} to start writing code. Good luck!`);
+    this.log(chalk.red(`\nOk, now everything is ${chalk.green('ready')} to start writing code. Good luck!`));
     this.log(yosay(`Don't forget to migrate test-db!`))
   }
 };
